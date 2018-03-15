efa542e815ad6aab764142afcb2f3aae98bfa98d
pratas <pratas@ua.pt>
2018-03-15 16:33:58 +0000

    info

--- demo/run.sh
+++ demo/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#
+# How to run:
+# ./run.sh <argument> [1;4]
+#
 ./eleanora-map -v -m 20 -n 4 -r 500 -k 18 -o ab.pos a.fasta b.fasta
 ./eleanora-visual -v -l $1 -w 36 -s 90 -c 1 -o out.svg ab.pos
